7ec0f25bf58451e20f2ce2a9319615dbf965fcea
Kenzium <135893200+Kenzium@users.noreply.github.com>
2025-03-25 21:35:39 +0100

    added change to client@4

--- client/Client.py
+++ client/Client.py
@@ -13,6 +13,7 @@ def delete_book(book):
 if __name__ == '__main__':
     print("CLIENT APPLICATION STARTING")
     print("now")
+    print("work")
     book_to_add = {"title": "Python 101", "author": "John Doe"}
     add_book(book_to_add)
     delete_book(book_to_add)
